7717b5a959eef7fa0701646c348b78624fea625e
Zhengyang Wang <zhengywa@amazon.com>
2025-08-18 15:10:35 -0700

    Transfomer LM: Implement the position-wise feed-forward network - Updating to using Linear

--- cs336_basics/model/swiglu.py
+++ cs336_basics/model/swiglu.py
@@ -1,6 +1,7 @@
 import torch
 import math
 from einops import einsum
+from cs336_basics.model import Linear
 
 
 class SwiGLU(torch.nn.Module):
@@ -12,58 +13,13 @@ class SwiGLU(torch.nn.Module):
         dtype: torch.dtype | None = None,
     ):
         super().__init__()
-        self.w1_weight = torch.nn.Parameter(
-            torch.empty(
-                d_ff,
-                d_model,
-                device=device,
-                dtype=dtype,
-            )
-        )
-        self.w2_weight = torch.nn.Parameter(
-            torch.empty(
-                d_model,
-                d_ff,
-                device=device,
-                dtype=dtype,
-            )
-        )
-        self.w3_weight = torch.nn.Parameter(
-            torch.empty(
-                d_ff,
-                d_model,
-                device=device,
-                dtype=dtype,
-            )
-        )
-        self.weight_init(self.w1_weight)
-        self.weight_init(self.w2_weight)
-        self.weight_init(self.w3_weight)
-
-
-    def weight_init(self, linear_weights):
-        d_out, d_in = linear_weights.shape
-        sigma = math.sqrt(2/(d_in+d_out))
-        torch.nn.init.trunc_normal_(
-            linear_weights,
-            mean=0.0,
-            std=sigma,
-            a=-3.0*sigma,
-            b=3.0*sigma,
-        )
+        self.linear1 = Linear(d_model, d_ff, device, dtype)
+        self.linear2 = Linear(d_ff, d_model, device, dtype)
+        self.linear3 = Linear(d_model, d_ff, device, dtype)
 
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        silu_input = einsum(
-            x, self.w1_weight,
-            "... d_model, d_ff d_model -> ... d_ff"
-        )
+        silu_input = self.linear1(x)
         silu_output = silu_input * torch.sigmoid(silu_input)
-        gated_output = silu_output * einsum(
-            x, self.w3_weight,
-            "... d_model, d_ff d_model -> ... d_ff"
-        )
-        return einsum(
-            gated_output, self.w2_weight,
-            "... d_ff, d_model d_ff -> ... d_model"
-        )
+        gated_output = silu_output * self.linear3(x)
+        return self.linear2(gated_output)
