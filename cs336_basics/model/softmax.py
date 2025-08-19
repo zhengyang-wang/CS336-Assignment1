52291d17c19b5bc1718cf2eceedc37063608d9ee
Zhengyang Wang <zhengywa@amazon.com>
2025-08-18 17:21:56 -0700

    small refactor

--- cs336_basics/model/softmax.py
+++ cs336_basics/model/softmax.py
@@ -2,6 +2,6 @@ import torch
 
 
 def softmax(x: torch.Tensor, dim: int) -> torch.Tensor:
-    x -= x.max(dim=dim, keepdim=True).values
-    x_exp = torch.exp(x)
+    x_shifted = x - x.max(dim=dim, keepdim=True).values
+    x_exp = torch.exp(x_shifted)
     return x_exp / x_exp.sum(dim=dim, keepdim=True)
--- cs336_basics/train/cross_entropy.py
+++ cs336_basics/train/cross_entropy.py
@@ -7,7 +7,7 @@ def cross_entropy_loss(
     inputs: Float[Tensor, " batch_size vocab_size"], targets: Int[Tensor, " batch_size"]
 ) -> Float[Tensor, ""]:
     batch_size, _ = inputs.shape
-    inputs -= inputs.max(dim=-1, keepdim=True).values
-    log_Z = torch.log(torch.sum(torch.exp(inputs), dim=-1))
-    loss = log_Z - inputs[torch.arange(batch_size), targets]
+    inputs_shifted = inputs - inputs.max(dim=-1, keepdim=True).values
+    log_Z = torch.log(torch.sum(torch.exp(inputs_shifted), dim=-1))
+    loss = log_Z - inputs_shifted[torch.arange(batch_size), targets]
     return loss.mean()
