235c98014b347ba3a769ca2511c2706850148460
Zhengyang Wang <zhengywa@amazon.com>
2025-08-21 14:52:23 -0700

    Training a Transformer LM: Implement gradient clipping - avoid math on tensors

--- cs336_basics/train/gradient_clipping.py
+++ cs336_basics/train/gradient_clipping.py
@@ -1,5 +1,4 @@
 import torch
-import math
 from collections.abc import Iterable
 
 
@@ -7,11 +6,11 @@ def gradient_clipping(
     parameters: Iterable[torch.nn.Parameter],
     max_l2_norm: float,
     eps: float = 1e-6,
-) -> None:
+) -> torch.Tensor:
     with torch.no_grad():
         l2_norm_per_grad = [torch.norm(p.grad) for p in parameters if p.grad is not None]
-        l2_norm_all_grads = math.sqrt(sum([l2 ** 2 for l2 in l2_norm_per_grad]))
-        
+        l2_norm_all_grads = torch.sqrt(torch.sum(torch.stack([n * n for n in l2_norm_per_grad]))).item()
+
         if l2_norm_all_grads >= max_l2_norm:
             for p in parameters:
                 if p.grad is None:
@@ -19,3 +18,5 @@ def gradient_clipping(
                 
                 grad = p.grad
                 grad.mul_(max_l2_norm / (l2_norm_all_grads + eps))
+
+        return l2_norm_all_grads
